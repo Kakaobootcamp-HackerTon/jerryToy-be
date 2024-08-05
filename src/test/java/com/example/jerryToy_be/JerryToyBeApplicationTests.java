7572d6834165a0391496d865581353b52216d99a
강유석 <gang-yuseog@gang-yuseog-ui-MacBookPro.local>
2024-08-05 19:50:07 +0900

    first commit

---
+++ src/test/java/com/example/jerryToy_be/JerryToyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jerryToy_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JerryToyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
