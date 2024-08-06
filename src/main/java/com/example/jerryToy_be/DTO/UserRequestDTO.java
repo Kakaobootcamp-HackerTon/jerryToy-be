53dd3928eed8c4ea4e64a7d43b509c6fefbc0f83
Lcrescent3es <crescent3es@gmail.com>
2024-08-06 09:15:54 +0900

    CRUD 작업중

---
+++ src/main/java/com/example/jerryToy_be/DTO/UserRequestDTO.java
@@ -0,0 +1,12 @@
+package com.example.jerryToy_be.DTO;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+
+public class UserRequestDTO {
+    private String username;
+    private String password;
+}
